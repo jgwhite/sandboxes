d8f186cfdd9af4a798a3411685933797209045c6
Jamie White <jamie@jgwhite.co.uk>
2014-10-05 13:19:51 +0200

    [ember] Upgrade to Ember CLI 0.1.0

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -6,6 +6,12 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'img-src': "'self' data:",
+      'script-src': "'self' 'unsafe-eval'",
+      'style-src': "'self' 'unsafe-inline'"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
